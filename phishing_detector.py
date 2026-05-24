247c01397b40cf252c4402b12cf3942ea4185672
Rahulnath-201 <727724eucs201@skcet.ac.in>
2026-05-24 13:01:24 +0530

    Update phishing_detector.py

--- phishing_detector.py
+++ phishing_detector.py
@@ -32,10 +32,10 @@ X_vectorized = vectorizer.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(
     X_vectorized,
     y,
-    test_size=0.2,
-    random_state=42
+    test_size=0.3,
+    random_state=42,
+    stratify=y
 )
-
 # =========================================
 # MODEL TRAINING
 # =========================================
